aac1e3be40a9da439a469852be36c825d57f531b
jhgoh <>
2009-08-21 07:38:23 +0000

    Obsolete plugins

--- HiggsAnalysis/DoublyChargedHiggs/src/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "HiggsAnalysis/DoublyChargedHiggs/src/Hpp2MuHepMCAnalyzer.h"
-#include "HiggsAnalysis/DoublyChargedHiggs/src/HppMuMuAnalyzer.h"
-
-DEFINE_FWK_MODULE(Hpp2MuHepMCAnalyzer);
-DEFINE_FWK_MODULE(HppMuMuAnalyzer);
