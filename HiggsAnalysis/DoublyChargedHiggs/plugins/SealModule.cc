e856d86e1b90f9fc95e3891b6b8777b8e8183f3e
jhgoh <>
2009-08-21 07:37:05 +0000

    First commit

---
+++ HiggsAnalysis/DoublyChargedHiggs/plugins/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "HiggsAnalysis/DoublyChargedHiggs/interface/FourMuonAnalyzer.h"
+#include "HiggsAnalysis/DoublyChargedHiggs/interface/PATMuonCandSelector.h"
+#include "HiggsAnalysis/DoublyChargedHiggs/interface/DimuonProducer.h"
+
+DEFINE_FWK_MODULE(FourMuonAnalyzer);
+DEFINE_FWK_MODULE(PATMuonCandSelector);
+DEFINE_FWK_MODULE(DimuonProducer);
