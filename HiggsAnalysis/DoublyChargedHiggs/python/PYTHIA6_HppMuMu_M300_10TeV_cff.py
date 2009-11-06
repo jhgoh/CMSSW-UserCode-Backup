12fc209e979473cdc200294aefda971eaaf33d91
jhgoh <>
2009-11-06 09:08:32 +0000

    Backport to CMSSW_2_2_X releases

--- HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M300_10TeV_cff.py
+++ HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M300_10TeV_cff.py
@@ -5,7 +5,7 @@ source = cms.Source("EmptySource")
 hppMass = '300.0'
 
 from Configuration.Generator.PythiaUESettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
+source = cms.Source("PythiaSource",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -42,10 +42,8 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     )
 )
 
-ProductionFilterSequence = cms.Sequence(generator)
-
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.2 $'),
+    version = cms.untracked.string('$Revision: 1.3 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/JHGoh/HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M300_10TeV_cff.py,v $'),
     annotation = cms.untracked.string('PYTHIA6-H++ to MuMu at 10TeV')
 )
--- HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M350_10TeV_cff.py
+++ HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M350_10TeV_cff.py
@@ -5,7 +5,7 @@ source = cms.Source("EmptySource")
 hppMass = '350.0'
 
 from Configuration.Generator.PythiaUESettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
+source = cms.Source("PythiaSource",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -42,10 +42,8 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     )
 )
 
-ProductionFilterSequence = cms.Sequence(generator)
-
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.2 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/JHGoh/HiggsAnalysis/DoublyChargedHiggs/python/PYTHIA6_HppMuMu_M350_10TeV_cff.py,v $'),
     annotation = cms.untracked.string('PYTHIA6-H++ to MuMu at 10TeV')
 )
