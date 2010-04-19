cdeaceff88b9285f3a37b8d85e92fb019eea3ae2
jhgoh <>
2010-04-19 14:01:10 +0000

    turned off standAloneMode by default

--- Validation/RPCRecHits/python/rpcPointValidation_cfi.py
+++ Validation/RPCRecHits/python/rpcPointValidation_cfi.py
@@ -4,7 +4,7 @@ dtVsRPCRecHitV = cms.EDAnalyzer("RPCPointVsRecHit",
     subDir = cms.string("RPC/RPCRecHitV/DTVsReco"),
     refHit = cms.InputTag("rpcPointProducer", "RPCDTExtrapolatedPoints"),
     recHit = cms.InputTag("rpcRecHits"),
-    standAloneMode = cms.untracked.bool(True),
+    standAloneMode = cms.untracked.bool(False),
     rootFileName = cms.untracked.string("")
 )
 
@@ -12,7 +12,7 @@ cscVsRPCRecHitV = cms.EDAnalyzer("RPCPointVsRecHit",
     subDir = cms.string("RPC/RPCRecHitV/CSCVsReco"),
     refHit = cms.InputTag("rpcPointProducer", "RPCCSCExtrapolatedPoints"),
     recHit = cms.InputTag("rpcRecHits"),
-    standAloneMode = cms.untracked.bool(True),
+    standAloneMode = cms.untracked.bool(False),
     rootFileName = cms.untracked.string("")
 )
 
@@ -20,7 +20,7 @@ trackVsRPCRecHitV = cms.EDAnalyzer("RPCPointVsRecHit",
     subDir = cms.string("RPC/RPCRecHitV/TrackVsReco"),
     refHit = cms.InputTag("rpcPointProducer", "RPCTrackExtrapolatedPoints"),
     recHit = cms.InputTag("rpcRecHits"),
-    standAloneMode = cms.untracked.bool(True),
+    standAloneMode = cms.untracked.bool(False),
     rootFileName = cms.untracked.string("")
 )
 
