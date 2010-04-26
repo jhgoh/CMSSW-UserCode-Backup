4f392518385530d04882fab8d6a3d906973973fa
jhgoh <>
2010-04-26 20:13:10 +0000

    new module DHGenEventFilter

--- HiggsAnalysis/DoublyChargedHiggs/plugins/SealModule.cc
+++ HiggsAnalysis/DoublyChargedHiggs/plugins/SealModule.cc
@@ -1,5 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HiggsAnalysis/DoublyChargedHiggs/interface/DileptonProducer.h"
+#include "HiggsAnalysis/DoublyChargedHiggs/interface/DHGenEventFilter.h"
 
 DEFINE_FWK_MODULE(DileptonProducer);
+DEFINE_FWK_MODULE(DHGenEventFilter);
