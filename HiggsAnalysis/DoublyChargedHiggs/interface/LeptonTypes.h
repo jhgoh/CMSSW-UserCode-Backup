336b17c15ef2649da2e2764701bb074c30b400a5
jhgoh <>
2010-06-14 15:30:50 +0000

    code cleaning

--- HiggsAnalysis/DoublyChargedHiggs/interface/LeptonTypes.h
+++ HiggsAnalysis/DoublyChargedHiggs/interface/LeptonTypes.h
@@ -1,3 +1,6 @@
+#ifndef HiggsAnalysis_DoublyChargedHiggs_LeptonTypes_H
+#define HiggsAnalysis_DoublyChargedHiggs_LeptonTypes_H
+
 #include <string>
 
 class LeptonTypes
@@ -5,25 +8,34 @@ class LeptonTypes
 public:
   enum
   {
-    None = 1, Electron = 2, Muon = 4, Tau = 8
+    None = 1<<0, Electron = 1<<1, Muon = 1<<2, Tau = 1<<3
   };
-  
-  static int getType(const std::string& leptonType)
+
+  static int getType(const std::string& leptonName)
   {
-    const char leptonInitial = leptonType[0];
-    if ( leptonInitial == 'e' ) return Electron;
-    else if ( leptonInitial == 'm' ) return Muon;
-    else if ( leptonInitial == 't' ) return Tau;
-    else return None;
+    const char leptonInitial = std::toupper(leptonName[0]);
+
+    switch ( leptonInitial )
+    {
+      case 'E': return Electron; break;
+      case 'M': return Muon    ; break;
+      case 'T': return Tau     ; break;
+      default : return None;
+    }
   };
 
   static int getType(const int pdgId)
   {
     const int absPdgId = abs(pdgId);
-    if ( absPdgId == 11 ) return Electron;
-    else if ( absPdgId == 13 ) return Muon;
-    else if ( absPdgId == 15 ) return Tau;
-    else return None;
+
+    switch ( absPdgId )
+    {
+      case 11: return Electron; break;
+      case 13: return Muon    ; break;
+      case 15: return Tau     ; break;
+      default: return None;
+    }
   };
 };
 
+#endif
