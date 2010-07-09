ad423d1b0188e18e2a4363beacc56277da8c0bba
jhgoh <>
2010-07-09 18:45:36 +0000

    JetMET analyzer not ready

--- HLTrigger/TPGAnalysis/plugins/SealModule.cc
+++ HLTrigger/TPGAnalysis/plugins/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/TPGAnalysis/plugins/MuonHLTAnalyzer.h"
-#include "HLTrigger/TPGAnalysis/plugins/JetMETHLTAnalyzer.h"
+//#include "HLTrigger/TPGAnalysis/plugins/JetMETHLTAnalyzer.h"
 
-DEFINE_FWK_MODULE(MuonHLTAnalyzer)
-DEFINE_FWK_MODULE(JetMETHLTAnalyzer)
+DEFINE_FWK_MODULE(MuonHLTAnalyzer);
+//DEFINE_FWK_MODULE(JetMETHLTAnalyzer)
