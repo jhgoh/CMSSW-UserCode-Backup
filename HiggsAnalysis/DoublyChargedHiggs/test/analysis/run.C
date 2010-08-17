e9286f8ca0e6d96361d41ed86b0437f65865ba47
jhgoh <>
2010-08-17 16:39:03 +0000

    Analyzer naming, loop over mass points

--- HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
+++ HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
@@ -1,24 +1,27 @@
 void run()
 {
-  gSystem->CompileMacro("PatAnalyzerBase.cc", "k");
-  gSystem->CompileMacro("PatAnalyzer.cc", "k");
-  PatAnalyzerEMu emuAna("result.root", false);
+  gSystem->CompileMacro("FWLiteAnalyzerBase.cc", "k");
+  gSystem->CompileMacro("FWLiteAnalyzer.cc", "k");
+  FWLiteAnalyzerEMu emuAna("result.root", false);
 
 //  gSystem.CompileMacro("FWLiteAnalyzer.cc");
 //  FWLiteAnalyzer* emuAna = new FWLiteAnalyzer();
 
 //  TString signalBase = "rfio:/castor/cern.ch/user/j/jhgoh/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Signal_EMEM/";
-  TString signalBase = "~/data/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/candSelection/";
-  emuAna.AddSignal("DBLH-m80", string(signalBase+"DBLH-m80"));
-  emuAna.AddSignal("DBLH-m70", string(signalBase+"DBLH-m70"));
-  emuAna.AddSignal("DBLH-m90", string(signalBase+"DBLH-m90"));
-  emuAna.AddSignal("DBLH-m100", string(signalBase+"DBLH-m100"));
-  emuAna.AddSignal("DBLH-m110", string(signalBase+"DBLH-m110"));
-  emuAna.AddSignal("DBLH-m120", string(signalBase+"DBLH-m120"));
-  emuAna.AddSignal("DBLH-m130", string(signalBase+"DBLH-m130"));
-  emuAna.AddSignal("DBLH-m140", string(signalBase+"DBLH-m140"));
-  emuAna.AddSignal("DBLH-m150", string(signalBase+"DBLH-m150"));
-  emuAna.AddSignal("DBLH-m160", string(signalBase+"DBLH-m160"));
+  string signalBase = "~/data/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/candSelection/";
+  const int nMassPoint = 10;
+  const double massPoints[nMassPoint] = {
+    70, 80, 90, 100, 110, 120, 130, 140, 150, 160
+  };
+
+  for ( int i=0; i<nMassPoint; ++i )
+  {
+    string datasetName = Form("DBLH-m%.0f", massPoints[i]);
+    string datasetPath = Form("%sDBLH-m%.0f", signalBase, massPoints[i]);
+    const double xsec = 1;
+
+    emuAna.AddSignal(datasetName, datasetPath, xsec);
+  }
 
   //emuAna.AddBackground("ZZ", false);
 
