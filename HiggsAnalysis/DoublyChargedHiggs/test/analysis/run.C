c34928b17072a69bc7f301f0de76f25a33095375
jhgoh <>
2010-08-20 15:38:22 +0000

    Cross section values and new data paths

--- HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
+++ HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
@@ -1,14 +1,15 @@
 void run()
 {
+  gSystem->CompileMacro("DHCrossSections.h", "k");
   gSystem->CompileMacro("FWLiteAnalyzerBase.cc", "k");
   gSystem->CompileMacro("FWLiteAnalyzer.cc", "k");
-  FWLiteAnalyzerEMu emuAna("result.root", false);
 
-//  gSystem.CompileMacro("FWLiteAnalyzer.cc");
-//  FWLiteAnalyzer* emuAna = new FWLiteAnalyzer();
+  DHCrossSections crossSectionAt7TeV(DHCrossSections::_7TeV);
+  FWLiteAnalyzerEMu emuAna("result.root", true);
 
-//  TString signalBase = "rfio:/castor/cern.ch/user/j/jhgoh/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Signal_EMEM/";
-  string signalBase = "~/data/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/candSelection/";
+  // Add signal samples
+  //string signalBase = "rfio:/castor/cern.ch/user/j/jhgoh/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Signal_EMEM/";
+  string signalBase = "~/data/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Signal_EMEM/";
   const int nMassPoint = 10;
   const double massPoints[nMassPoint] = {
     70, 80, 90, 100, 110, 120, 130, 140, 150, 160
@@ -16,14 +17,20 @@ void run()
 
   for ( int i=0; i<nMassPoint; ++i )
   {
-    string datasetName = Form("DBLH-m%.0f", massPoints[i]);
+    string datasetName = Form("MC_Signal_EMEM/DBLH-m%.0f", massPoints[i]);
     string datasetPath = Form("%sDBLH-m%.0f", signalBase, massPoints[i]);
-    const double xsec = 1;
+    const double xsec = crossSectionAt7TeV.Eval(massPoints[i]);
 
-    emuAna.AddSignal(datasetName, datasetPath, xsec);
+    emuAna.AddMCSample(datasetName, datasetPath, xsec/36, 10000);
   }
 
-  //emuAna.AddBackground("ZZ", false);
+  // Add background samples
+  //string backgroundBase = "rfio:/castor/cern.ch/user/j/jhgoh/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Background_EMEM/";
+  string backgroundBase = "~/data/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Background_EMEM/";
+
+  emuAna.AddMCSample("MC_Background_EMEM/TTbar", backgroundBase+"TTbar", 94.3*1000, 500000);
+  //emuAna.AddMCSample("TTbar_2l", backgroundBase+"TTbar_2l", 17.3*1000, 1000000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZZ", backgroundBase+"ZZ", 4.3*1000, 100000);
 
   emuAna.ListDataFiles();
   emuAna.ProcessEvent();
