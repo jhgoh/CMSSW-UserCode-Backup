9cbac8860b054b603d90838bd870a6e32678c73e
jhgoh <>
2010-08-22 16:50:04 +0000

    Z+Jet samples

--- HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
+++ HiggsAnalysis/DoublyChargedHiggs/test/analysis/run.C
@@ -5,7 +5,7 @@ void run()
   gSystem->CompileMacro("FWLiteAnalyzer.cc", "k");
 
   DHCrossSections crossSectionAt7TeV(DHCrossSections::_7TeV);
-  FWLiteAnalyzerEMu emuAna("result.root", true);
+  FWLiteAnalyzerEMu emuAna("result.root", false);
 
   // Add signal samples
   //string signalBase = "rfio:/castor/cern.ch/user/j/jhgoh/HiggsAnalysis/DoublyChargedHiggs/CMSSW_3_5_6_patch1/MC_Signal_EMEM/";
@@ -21,7 +21,8 @@ void run()
     string datasetPath = Form("%sDBLH-m%.0f", signalBase, massPoints[i]);
     const double xsec = crossSectionAt7TeV.Eval(massPoints[i]);
 
-    emuAna.AddMCSample(datasetName, datasetPath, xsec/36, 10000);
+    //emuAna.AddMCSample(datasetName, datasetPath, xsec/36, 10000/36);
+    emuAna.AddMCSample(datasetName, datasetPath, xsec, 10000);
   }
 
   // Add background samples
@@ -32,6 +33,28 @@ void run()
   //emuAna.AddMCSample("TTbar_2l", backgroundBase+"TTbar_2l", 17.3*1000, 1000000);
   emuAna.AddMCSample("MC_Background_EMEM/ZZ", backgroundBase+"ZZ", 4.3*1000, 100000);
 
-  emuAna.ListDataFiles();
+  //emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt0to15"   , backgroundBase+"ZeeJet_Pt0to15"   , 4.434e+03*1000, 200000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt15to20"  , backgroundBase+"ZeeJet_Pt15to20"  , 1.454e+02*1000, 200000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt20to30"  , backgroundBase+"ZeeJet_Pt20to30"  , 1.318e+02*1000, 150000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt30to50"  , backgroundBase+"ZeeJet_Pt30to50"  , 8.438e+01*1000, 150000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt50to80"  , backgroundBase+"ZeeJet_Pt50to80"  , 3.235e+01*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt80to120" , backgroundBase+"ZeeJet_Pt80to120" , 9.981e+00*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt120to170", backgroundBase+"ZeeJet_Pt120to170", 2.760e+00*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt170to230", backgroundBase+"ZeeJet_Pt170to230", 7.241e-01*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt230to300", backgroundBase+"ZeeJet_Pt230to300", 1.946e-01*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZeeJet_Pt300toInf", backgroundBase+"ZeeJet_Pt300toInf", 7.627e-02*1000, 100000);
+
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt0to15"   , backgroundBase+"ZmumuJet_Pt0to15"   , 4.434e+03*1000, 200000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt15to20"  , backgroundBase+"ZmumuJet_Pt15to20"  , 1.454e+02*1000, 200000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt20to30"  , backgroundBase+"ZmumuJet_Pt20to30"  , 1.318e+02*1000, 200000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt30to50"  , backgroundBase+"ZmumuJet_Pt30to50"  , 8.438e+01*1000, 150000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt50to80"  , backgroundBase+"ZmumuJet_Pt50to80"  , 3.235e+01*1000, 150000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt80to120" , backgroundBase+"ZmumuJet_Pt80to120" , 9.981e+00*1000, 100000);
+  emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt120to170", backgroundBase+"ZmumuJet_Pt120to170", 2.760e+00*1000, 100000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt170to230", backgroundBase+"ZmumuJet_Pt170to230", 7.241e-01*1000, 100000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt230to300", backgroundBase+"ZmumuJet_Pt230to300", 1.946e-01*1000, 100000);
+  //emuAna.AddMCSample("MC_Background_EMEM/ZmumuJet_Pt300toInf", backgroundBase+"ZmumuJet_Pt300toInf", 7.627e-02*1000, 100000);
+
+//  emuAna.ListDataFiles();
   emuAna.ProcessEvent();
 }
