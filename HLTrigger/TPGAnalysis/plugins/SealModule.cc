22a4b0306cd4116ed203c54855f0dead656dd3f5
jhgoh <>
2010-08-23 21:09:59 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-02-00'.
    Sprout from master 2010-08-23 21:09:58 UTC jhgoh 'first commit'
    Delete:
        .admin/developers
        CosmicAnalysis/CosmicAnalyzer/BuildFile
        CosmicAnalysis/CosmicAnalyzer/interface/MuonRPCAnalyzer.h
        CosmicAnalysis/CosmicAnalyzer/interface/MuonTimingAnalyzer.h
        CosmicAnalysis/CosmicAnalyzer/interface/MuonTrackAnalyzer.h
        CosmicAnalysis/CosmicAnalyzer/plugins/BuildFile
        CosmicAnalysis/CosmicAnalyzer/plugins/MuonRPCAnalyzer.cc
        CosmicAnalysis/CosmicAnalyzer/plugins/MuonTimingAnalyzer.cc
        CosmicAnalysis/CosmicAnalyzer/plugins/MuonTrackAnalyzer.cc
        CosmicAnalysis/CosmicAnalyzer/plugins/SealModule.cc
        CosmicAnalysis/CosmicAnalyzer/test/cosmicAnalysis_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/source_beamSplash_121943_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/source_beamSplash_121964_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/source_beamSplash_121993_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/source_beamSplash_976_993_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/timingAnalysis_cfg.py
        CosmicAnalysis/CosmicAnalyzer/test/viewResult.C
        HLTrigger/TPGAnalysis/BuildFile.xml
        HLTrigger/TPGAnalysis/interface/TriggerRateAnalyzer.h
        HLTrigger/TPGAnalysis/plugins/BuildFile.xml
        HLTrigger/TPGAnalysis/plugins/MuonHLTAnalyzer.cc
        HLTrigger/TPGAnalysis/plugins/MuonHLTAnalyzer.h
        HLTrigger/TPGAnalysis/plugins/SealModule.cc
        HLTrigger/TPGAnalysis/plugins/TriggerRateAnalyzer.cc
        HLTrigger/TPGAnalysis/python/muonHLTAnalyzer_cfi.py
        HLTrigger/TPGAnalysis/python/triggerRateAnalyzer_cfi.py
        HLTrigger/TPGAnalysis/test/muonHLTAnalysis_cfg.py
        Validation/RPCRecHits/BuildFile.xml
        Validation/RPCRecHits/interface/RPCPointVsRecHit.h
        Validation/RPCRecHits/interface/RPCRecHitValid.h
        Validation/RPCRecHits/python/postValidation_cfi.py
        Validation/RPCRecHits/python/rpcPointValidation_cfi.py
        Validation/RPCRecHits/python/rpcRecHitValidation_cfi.py
        Validation/RPCRecHits/src/RPCPointVsRecHit.cc
        Validation/RPCRecHits/src/RPCRecHitValid.cc
        Validation/RPCRecHits/src/SealModule.cc
        Validation/RPCRecHits/test/rpcRecHitValidation_cfg.py
        Validation/Tools/dumpRoot
        Validation/Tools/makeValidation
        Validation/Tools/publishValidation

--- HLTrigger/TPGAnalysis/plugins/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "HLTrigger/TPGAnalysis/interface/TriggerRateAnalyzer.h"
-#include "HLTrigger/TPGAnalysis/plugins/MuonHLTAnalyzer.h"
-//#include "HLTrigger/TPGAnalysis/plugins/JetMETHLTAnalyzer.h"
-
-DEFINE_FWK_MODULE(TriggerRateAnalyzer);
-DEFINE_FWK_MODULE(MuonHLTAnalyzer);
-//DEFINE_FWK_MODULE(JetMETHLTAnalyzer)
