bd7317274e319aed1c7ac01c1a92c6d8c9a52961
jhgoh <>
2010-08-24 09:29:55 +0000

    Robust way to modify eventContent

--- HiggsAnalysis/DoublyChargedHiggs/python/eventContent_cff.py
+++ HiggsAnalysis/DoublyChargedHiggs/python/eventContent_cff.py
@@ -5,7 +5,7 @@ patDHEventContent = patEventContent+patExtraAodEventContent
 
 # drop default cleanPatMuons and cleanPatElectrons
 patDHEventContentWithLeptonFilter = patDHEventContent[:]
-patDHEventContentWithLeptonFilter.remove('keep *_cleanPatElectrons_*_*')
-patDHEventContentWithLeptonFilter.remove('keep *_cleanPatMuons_*_*')
-patDHEventContentWithLeptonFilter.append('keep *_goodPatElectrons_*_*')
-patDHEventContentWithLeptonFilter.append('keep *_goodPatMuons_*_*')
+patDHEventContentWithLeptonFilter.remove('keep *_cleanPatElectrons*_*_*')
+patDHEventContentWithLeptonFilter.remove('keep *_cleanPatMuons*_*_*')
+patDHEventContentWithLeptonFilter.append('keep *_goodPatElectrons*_*_*')
+patDHEventContentWithLeptonFilter.append('keep *_goodPatMuons*_*_*')
