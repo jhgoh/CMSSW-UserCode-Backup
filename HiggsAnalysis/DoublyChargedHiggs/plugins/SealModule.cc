94a695ba8a2e24587425287d91fda870e3a43489
jhgoh <>
2010-09-10 10:49:00 +0000

    Added MultipleCandCounterFilter

--- HiggsAnalysis/DoublyChargedHiggs/plugins/SealModule.cc
+++ HiggsAnalysis/DoublyChargedHiggs/plugins/SealModule.cc
@@ -2,7 +2,9 @@
 
 #include "HiggsAnalysis/DoublyChargedHiggs/plugins/DileptonProducer.h"
 #include "HiggsAnalysis/DoublyChargedHiggs/plugins/DHGenEventFilter.h"
+#include "HiggsAnalysis/DoublyChargedHiggs/plugins/MultipleCandCounterFilter.h"
 
 DEFINE_FWK_MODULE(DileptonProducer);
 DEFINE_FWK_MODULE(DHGenEventFilter);
+DEFINE_FWK_MODULE(MultipleCandCounterFilter);
 
