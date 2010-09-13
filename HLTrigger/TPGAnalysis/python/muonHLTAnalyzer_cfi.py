700a0479460af2192f44ac0d4f973f6441dfc18f
jhgoh <>
2010-09-13 12:59:29 +0000

    L1 cuts and reco cut object renaming

--- HLTrigger/TPGAnalysis/python/muonHLTAnalyzer_cfi.py
+++ HLTrigger/TPGAnalysis/python/muonHLTAnalyzer_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 muonHLTAnalyzer = cms.EDAnalyzer("MuonHLTAnalyzer",
   interestedFilterName = cms.string("HLT_Mu9"),
   cut = cms.PSet(
-    minEt = cms.double(15),
+    l1MinEt = cms.double(7),
+    l1Quality = cms.vuint32(4,5,6),
+    workingPointEt = cms.double(15),
     maxL1DeltaR = cms.double(0.4),
     maxHLTDeltaR = cms.double(0.3)
   ),
