819605317ef6855d1ae05badd32b1caff3f49b5e
jhgoh <>
2010-09-13 12:59:05 +0000

    L1 Et cuts and reco cut object renaming

--- HLTrigger/TPGAnalysis/python/jetHLTAnalyzer_cfi.py
+++ HLTrigger/TPGAnalysis/python/jetHLTAnalyzer_cfi.py
@@ -4,7 +4,8 @@ jetHLTAnalyzer = cms.EDAnalyzer("JetHLTAnalyzer",
     interestedFilterName = cms.string("hlt1jet50U"),
     recoJet = cms.InputTag("ak5CaloJets"),
     cut = cms.PSet(
-        minEt = cms.double(50),
+        l1MinEt = cms.double(30),
+        workingPointEt = cms.double(50),
         maxL1DeltaR = cms.double(0.4),
         maxHLTDeltaR = cms.double(0.3)
     ),
