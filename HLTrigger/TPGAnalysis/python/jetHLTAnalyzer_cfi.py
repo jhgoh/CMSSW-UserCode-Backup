eb8e01dff4657242f9fd36f8f3d904bdcde11b75
jhgoh <>
2010-09-17 15:40:42 +0000

    minimum Et cut, change of histogramming, matching parameters

--- HLTrigger/TPGAnalysis/python/jetHLTAnalyzer_cfi.py
+++ HLTrigger/TPGAnalysis/python/jetHLTAnalyzer_cfi.py
@@ -4,10 +4,11 @@ jetHLTAnalyzer = cms.EDAnalyzer("JetHLTAnalyzer",
     interestedFilterName = cms.string("hlt1jet50U"),
     recoJet = cms.InputTag("ak5CaloJets"),
     cut = cms.PSet(
+        recoMinEt = cms.double(20),
         l1MinEt = cms.double(30),
-        workingPointEt = cms.double(50),
-        maxL1DeltaR = cms.double(0.4),
-        maxHLTDeltaR = cms.double(0.3)
+        workingPointEt = cms.double(56),
+        maxL1DeltaR = cms.double(0.5),
+        maxHLTDeltaR = cms.double(0.1)
     ),
     JetIDParams = cms.PSet(
         useRecHits      = cms.bool(True),
