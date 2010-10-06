40f01d18f31cf2f53967e2fcdb7e6caec67b6cdd
jhgoh <>
2010-10-06 15:14:54 +0000

    Added jet trigger paths, included high lumi HLT paths

--- HLTrigger/TPGAnalysis/python/triggerRateAnalyzer_cfi.py
+++ HLTrigger/TPGAnalysis/python/triggerRateAnalyzer_cfi.py
@@ -12,11 +12,33 @@ muonHLTRateAnalyzer = cms.EDAnalyzer("TriggerRateAnalyzer",
         "HLT_Mu3",
         "HLT_IsoMu3",
         "HLT_Mu5",
+        "HLT_Mu7",
         "HLT_Mu9",
+        "HLT_Mu11",
+        "HLT_Mu15",
         "HLT_L1DoubleMuOpen",
         "HLT_L2DoubleMu0",
         "HLT_DoubleMu0",
         "HLT_DoubleMu3"
+    )
+)
+
+jetHLTRateAnalyzer = cms.EDAnalyzer("TriggerRateAnalyzer",
+    myTrigNames = cms.vstring(
+        "HLT_L1Jet6U",
+        "HLT_L1Jet10U",
+        "HLT_Jet15U",
+        "HLT_Jet30U",
+        "HLT_Jet50U",
+        "HLT_Jet70U",
+        "HLT_Jet70U_v1",
+        "HLT_Jet70U_v2",
+        "HLT_Jet100U",
+        "HLT_Jet100U_v1",
+        "HLT_Jet100U_v2",
+        "HLT_Jet140U",
+        "HLT_Jet140U_v1",
+        "HLT_Jet140U_v2"
     )
 )
 
