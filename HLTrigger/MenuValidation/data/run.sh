7ba8785be2e52574c7eccbc709739c8ba86adbb6
jhgoh <>
2011-01-23 04:04:41 +0000

    roll back to previous scheme, adding offline DQM step

--- HLTrigger/MenuValidation/data/run.sh
+++ HLTrigger/MenuValidation/data/run.sh
@@ -5,16 +5,18 @@
 ## in the crab is working properly
 #DATASETNAME=$1
 #MENUNAME=$2
-
 DATASETNAME=`cat ${RUNTIME_AREA}/CMSSW.sh | grep -m1 -o 'PrimaryDataset=\(.*\)' | sed -e 's/PrimaryDataset=//g'`
 MENUNAME=NewMenuTest
-OUTPUTKEEPLIST=outputA.root
 
-cmsRun -j $RUNTIME_AREA/crab_fjr_$NJob.xml -p run_cfg.py
+mkdir log
+
+## Start HLT-Rerunning
+cmsRun -j $RUNTIME_AREA/crab_fjr_$NJob.xml -p pset.py >& log/hlt.log
 echo =====================================================
 ls -l output*.root
 echo =====================================================
 
+## Test online DQM
 mkdir online
 
 for FNAME in outputA outputHLTMON outputExpress; do
@@ -22,10 +24,10 @@ for FNAME in outputA outputHLTMON outputExpress; do
   FTYPE=`echo $FNAME | sed 's/output//g'`
   export WORKFLOW="/${DATASETNAME}/${MENUNAME}/online${FTYPE}"
 
-  cmsRun onlineDQM_cfg.py
+  cmsRun onlineDQM_cfg.py >& log/onlineDQM_${FTYPE}.log
 
+  ## Keep DQM output to the safe place
   mv DQM_*.root online/
-#  rm -f $FNAME.root
 done
 
 for FNAME in outputHLTDQM; do
@@ -33,18 +35,42 @@ for FNAME in outputHLTDQM; do
   FTYPE=`echo $FNAME | sed 's/output//g'`
   export WORKFLOW="/${DATASETNAME}/${MENUNAME}/online${FTYPE}"
 
-  cmsRun convert_cfg.py
-  cmsRun onlineDQM_dat_cfg.py
-
-  mv DQM_*.root online/
-#  rm -f $FNAME.root
+  cmsRun convert_cfg.py >& log/onlineDQM_convert_${FTYPE}.log
+  cmsRun onlineDQM_dat_cfg.py >& log/onlineDQM_${FTYPE}.log
   rm -f inputfile.dat
+
+  ## Keep DQM output to the safe place
+  mv DQM_*.root online/
 done
 
-rm -f inputfile.root
-for FILE in $OUTPUTKEEPLIST; do
-  echo mv $FILE online/
+## Do the reco+offlineDQM step
+mkdir offline
+
+for FNAME in outputA outputHLTMON outputExpress; do
+  FTYPE=`echo $FNAME | sed 's/output//g'`
+  cmsDriver.py step2 -s RAW2DIGI,RECO,DQM -n -1 --eventcontent DQM \
+    --data --conditions auto:com10 --geometry Ideal \
+    --filein file:${FNAME}.root --fileout step2_${FTYPE}.root --no_exec --python_filename=step2_cfg.py
+  ## Temporary remove jetMETHLTOfflineSource due to DQM crash
+  cat >> step2_cfg.py <<EOF
+process.DQMOffline.remove(process.jetMETHLTOfflineSource)
+EOF
+  cmsRun step2_cfg.py >& log/offlineDQM_${FTYPE}.log
+#  cmsDriver.py step3 -s HARVESTING:dqmHarvesting \
+#    --data --conditions auto:com10 \
+#    --filein file:step2_${NAME}.root
+  mv step2_${FTYPE}.root offline/
 done
-tar czf online.tgz online
-rm -f *.root
+
+## Everything done. Clear up output
+rm -f inputfile.root
+tar czf log.tgz log && rm -rf log
+tar czf online.tgz online && rm -rf online
+tar czf offline.tgz offline && rm -rf offline
+
+## Make dummy files to save disk space
+for FNAME in *.root; do
+  rm -f $FNAME
+  echo "DUMMY" > $FNAME
+done
 
