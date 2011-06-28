1fcfd0add00e2a116d55051c0b453298d91b2c27
jhgoh <>
2011-06-28 15:43:24 +0000

    Going to minimal version

--- HLTrigger/TPGAnalysis/plugins/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "HLTrigger/TPGAnalysis/interface/TriggerRateAnalyzer.h"
-#include "HLTrigger/TPGAnalysis/interface/MuonHLTAnalyzer.h"
-#include "HLTrigger/TPGAnalysis/interface/JetHLTAnalyzer.h"
-
-DEFINE_FWK_MODULE(TriggerRateAnalyzer);
-DEFINE_FWK_MODULE(MuonHLTAnalyzer);
-DEFINE_FWK_MODULE(JetHLTAnalyzer);
